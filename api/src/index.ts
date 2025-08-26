f79ad7a1e9fd96b89d197e755fddad5e44c80a53
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2025-08-27 03:21:32 +0530

    cors issue fixed

--- api/src/index.ts
+++ api/src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-const allowedOrigins = ["http://localhost:5173","https://event-management-ticket-distribution-ieit.onrender.com"];
+const allowedOrigins = ["http://localhost:5173","https://event-management-ticket-distributio.vercel.app"];
 app.use(
   cors({
     origin: (origin, callback) => {
